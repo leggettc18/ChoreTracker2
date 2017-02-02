a2c8ffce62a1152dfcae4eda6552dc7cbc0b85da
Joey <geno8702@gmail.com>
2017-02-02 14:32:36 +0000

    basic frontend attached to devise on backend

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise', '~> 4.2'
 
+gem 'bootstrap-sass', '~> 3.3.6'
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'default/index'
+
   devise_for :parents
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get 'application/index'
+  
+  root 'default#index'
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
