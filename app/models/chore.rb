1495f68f21232f8b98784b15ae9d44e4a1c0f7d7
Christopher Leggett <leggettc13@students.ecu.edu>
2017-02-15 17:49:22 +0000

    Added some tests and scopes for Chores

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,6 +1,9 @@
 class Chore < ApplicationRecord
     belongs_to :child
     belongs_to :parent
+    scope :overdue, -> { where('due_date < ?', DateTime.now) }
+    scope :due_this_week, -> { where(:due_date => DateTime.now..DateTime.now + 7) }
+    scope :made_by_parent, lambda {|parent| where(:parent_id => parent.id) }
     enum repeat_type: ["No Repeat", "Daily", "Weekly", "Monthly"]
     enum repeat_weekday: ["None", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     attr_accessor :repeat_weekday
--- test/models/chore_test.rb
+++ test/models/chore_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ChoreTest < ActiveSupport::TestCase
+  include Devise::Test::IntegrationHelpers
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @chore = chores(:one)
+  end
+  
+  test "overdue" do
+    assert_includes Chore.overdue, chores(:two)
+    refute_includes Chore.overdue, chores(:one)
+  end
+  
+  test "due_this_week" do
+    assert_includes Chore.due_this_week, chores(:one)
+    refute_includes Chore.due_this_week, chores(:two)
+  end
+  
+  test "made_by_current_parent" do
+    sign_in parents(:one)
+    assert_includes Chore.made_by_parent(parents(:one)), chores(:one)
+    assert_includes Chore.made_by_parent(parents(:one)), chores(:two)
+    refute_includes Chore.made_by_parent(parents(:one)), chores(:three)
+  end
 end
