ec7eda2bbe708be243ce721d0e8425793998e4bb
Atlantice <iceatlan@gmail.com>
2017-03-11 05:54:07 +0000

    Prototyping chore implementation/design

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -74,3 +74,69 @@ class ChoresController < ApplicationController
       params.fetch(:chore, {}).permit(:name, :description, :coins, :child_id, :due_date, :repeat_type, :repeat_data, :parent_id, :needs_approval)
     end
 end
+
+# class ChoresController < ApplicationController
+# 	before_action :find_chore, only: [:show, :edit, :update, :destroy]
+# # 	before_action :authenticate_user!, except: [:index]
+
+# 	def index
+# 		# The if conditional ensures that the current_user value is not nil. If the if conditional
+# 		# is not there, there will be an error because the id method is being called on a nil value.
+# 		# if user_signed_in?
+# 			# Selects the items where the user's id is the same as the current_user.
+# 			# Selects only the checklist items for the current_user.
+# 			@chores = Chore.order("created_at DESC")
+# 		# end	
+# 	end
+
+# 	def show
+# 	end
+
+# 	def new
+# 	      @chore = Chore.new
+# 		# @chore = current_user.chores.build
+# 	end
+
+# 	def create
+# 		@chore = Chore.new(chore_params)
+
+# 		if @chore.save
+# 			redirect_to @chore
+# 		else
+# 			render 'new'
+# 		end
+# 	end
+
+# 	def edit
+# 	end
+
+# 	def update
+# 		if @chore.update(chore_params)
+# 			redirect_to @chore
+# 		else
+# 			render 'edit'
+# 		end
+# 	end
+
+# 	def destroy
+# 		@chore.destroy
+# 		redirect_to @chore
+# 	end
+
+# 	def complete
+# 		@chore = Chore.find(params[:id])
+# 		@chore.update_attribute(:completed_at, Time.now)
+# 		redirect_to @chore, notice: "Chore successfully completed!"
+# 	end
+
+# 	private
+# 		def chore_params
+# 			params.require(:chore).permit(:name, :description)
+# 		end
+
+# 		def find_chore
+# 			@chore = Chore.find(params[:id])
+# 			redirect_to @chore, notice: "Chore successfully completed!"
+
+# 		end
+# end
