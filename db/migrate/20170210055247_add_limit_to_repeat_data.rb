99a1bb21dc8e4db684e61d4b76ee385b3445d6b5
Atlantice <iceatlan@gmail.com>
2017-03-14 11:42:56 +0000

    temporarily moved add_limit migration due to errors in deployment

--- db/migrate/20170210055247_add_limit_to_repeat_data.rb
+++
@@ -1,9 +0,0 @@
-class AddLimitToRepeatData < ActiveRecord::Migration[5.0]
-  def up
-    change_column(:chores, :repeat_data, :integer, :limit => 31)
-  end
-  
-  def down
-    change_column(:chores, :repeat_data, :integer)
-  end
-end
---
+++ test/Test/20170210055247_add_limit_to_repeat_data.rb
@@ -0,0 +1,9 @@
+# class AddLimitToRepeatData < ActiveRecord::Migration[5.0]
+#   def up
+#     change_column(:chores, :repeat_data, :integer, :limit => 31)
+#   end
+  
+#   def down
+#     change_column(:chores, :repeat_data, :integer)
+#   end
+# end
