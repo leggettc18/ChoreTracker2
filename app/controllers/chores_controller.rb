14f8f7065bd592922e66a354bf0dac09cd394246
Atlantice <iceatlan@gmail.com>
2017-03-14 03:41:59 +0000

    Bootstrapifying Forms and Updates to Chores

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,142 +1,155 @@
 class ChoresController < ApplicationController
-  before_filter :authenticate_parent!
-  before_action :set_chore, only: [:show, :edit, :update, :destroy]
+	before_action :find_chore, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_parent!, except: [:index]
 
-  # GET /chores
-  # GET /chores.json
-  def index
-    @chores = Chore.all
-  end
+	def index
 
-  # GET /chores/1
-  # GET /chores/1.json
-  def show
-    @chore = Chore.find(params[:id])
-  end
+    @chores = Chore.all.order("created_at DESC")
 
-  # GET /chores/new
-  def new
-    @chore = Chore.new
-  end
+	end
 
-  # GET /chores/1/edit
-  def edit
-  end
+	def show
+	end
 
-  # POST /chores
-  # POST /chores.json
-  def create
-    @chore = Chore.new(chore_params)
+	def new
+		@chore = current_parent.chores.build
+	end
 
-    respond_to do |format|
-      if @chore.save
-        format.html { redirect_to @chore, notice: 'Chore was successfully created.' }
-        format.json { render :show, status: :created, location: @chore }
-      else
-        format.html { render :new }
-        format.json { render json: @chore.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	def create
+		@chore = current_parent.chores.build(chore_params)
 
-  # PATCH/PUT /chores/1
-  # PATCH/PUT /chores/1.json
-  def update
-    respond_to do |format|
-      if @chore.update(chore_params)
-        format.html { redirect_to @chore, notice: 'Chore was successfully updated.' }
-        format.json { render :show, status: :ok, location: @chore }
-      else
-        format.html { render :edit }
-        format.json { render json: @chore.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		if @chore.save
+			redirect_to @chore
+		else
+			render 'new'
+		end
+	end
 
-  # DELETE /chores/1
-  # DELETE /chores/1.json
-  def destroy
-    @chore.destroy
-    respond_to do |format|
-      format.html { redirect_to chores_url, notice: 'Chore was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+	def edit
+	end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_chore
-      @chore = Chore.find(params[:id])
-    end
+	def update
+		if @chore.update(chore_params)
+			redirect_to @chore
+		else
+			render 'edit'
+		end
+	end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def chore_params
-      params.fetch(:chore, {}).permit(:name, :description, :coins, :child_id, :due_date, :repeat_type, :repeat_data, :parent_id, :needs_approval)
-    end
+	def destroy
+		@chore.destroy
+		redirect_to root_path
+	end
+
+	def complete
+		@chore = Chore.find(params[:id])
+		@chore.update_attribute(:completed_at, Time.now)
+		redirect_to @chore, notice: "Chore successfully completed!"
+	end
+
+	private
+		def chore_params
+			params.require(:chore).permit(:name, :description)
+		end
+
+		def find_chore
+			@chore = Chore.find(params[:id])
+		end
 end
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # class ChoresController < ApplicationController
-# 	before_action :find_chore, only: [:show, :edit, :update, :destroy]
-# # 	before_action :authenticate_user!, except: [:index]
+#   before_filter :authenticate_parent!
+#   before_action :set_chore, only: [:show, :edit, :update, :destroy]
 
-# 	def index
-# 		# The if conditional ensures that the current_user value is not nil. If the if conditional
-# 		# is not there, there will be an error because the id method is being called on a nil value.
-# 		# if user_signed_in?
-# 			# Selects the items where the user's id is the same as the current_user.
-# 			# Selects only the checklist items for the current_user.
-# 			@chores = Chore.order("created_at DESC")
-# 		# end	
-# 	end
+#   # GET /chores
+#   # GET /chores.json
+#   def index
+#     @chores = Chore.all
+#   end
 
-# 	def show
-# 	end
+#   # GET /chores/1
+#   # GET /chores/1.json
+#   def show
+#     @chore = Chore.find(params[:id])
+#   end
 
-# 	def new
-# 	      @chore = Chore.new
-# 		# @chore = current_user.chores.build
-# 	end
+#   # GET /chores/new
+#   def new
+#     @chore = Chore.new
+#   end
 
-# 	def create
-# 		@chore = Chore.new(chore_params)
+#   # GET /chores/1/edit
+#   def edit
+#   end
 
-# 		if @chore.save
-# 			redirect_to @chore
-# 		else
-# 			render 'new'
-# 		end
-# 	end
+#   # POST /chores
+#   # POST /chores.json
+#   def create
+#     @chore = Chore.new(params.require(:chore).permit(:name, :description))
+#     if @chore.save
+#       redirect_to @chore
+#     else
+#       render 'new'
+#     end
+#     # @chore = Chore.new(chore_params)
 
-# 	def edit
-# 	end
+#     # respond_to do |format|
+#     #   if @chore.save
+#     #     format.html { redirect_to @chore, notice: 'Chore was successfully created.' }
+#     #     format.json { render :show, status: :created, location: @chore }
+#     #   else
+#     #     format.html { render :new }
+#     #     format.json { render json: @chore.errors, status: :unprocessable_entity }
+#     #   end
+#     # end
+#   end
 
-# 	def update
-# 		if @chore.update(chore_params)
-# 			redirect_to @chore
-# 		else
-# 			render 'edit'
-# 		end
-# 	end
+#   # PATCH/PUT /chores/1
+#   # PATCH/PUT /chores/1.json
+#   def update
+#     respond_to do |format|
+#       if @chore.update(chore_params)
+#         format.html { redirect_to @chore, notice: 'Chore was successfully updated.' }
+#         format.json { render :show, status: :ok, location: @chore }
+#       else
+#         format.html { render :edit }
+#         format.json { render json: @chore.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
 
-# 	def destroy
-# 		@chore.destroy
-# 		redirect_to @chore
-# 	end
+#   # DELETE /chores/1
+#   # DELETE /chores/1.json
+#   def destroy
+#     @chore.destroy
+#     respond_to do |format|
+#       format.html { redirect_to chores_url, notice: 'Chore was successfully destroyed.' }
+#       format.json { head :no_content }
+#     end
+#   end
 
-# 	def complete
-# 		@chore = Chore.find(params[:id])
-# 		@chore.update_attribute(:completed_at, Time.now)
-# 		redirect_to @chore, notice: "Chore successfully completed!"
-# 	end
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+#     def set_chore
+#       @chore = Chore.find(params[:id])
+#     end
 
-# 	private
-# 		def chore_params
-# 			params.require(:chore).permit(:name, :description)
-# 		end
-
-# 		def find_chore
-# 			@chore = Chore.find(params[:id])
-# 			redirect_to @chore, notice: "Chore successfully completed!"
-
-# 		end
+#     # Never trust parameters from the scary internet, only allow the white list through.
+#     def chore_params
+#       params.fetch(:chore, {}).permit(:name, :description, :coins, :child_id, :due_date, :repeat_type, :repeat_data, :parent_id, :needs_approval)
+#     end
 # end
